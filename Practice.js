1d0ef37e6cd0d2142a476c9302950f31401a736d
Dirk <noyahrdirk@gmail.com>
2022-05-18 19:29:06 -0400

    Practice 1 Done

--- Practice.js
+++ Practice.js
@@ -5,11 +5,18 @@
             ☑️ Create a number variable "myAge" with your age inside
     */
 
+            const myFirstName = 'Dirk';
+            const myAge = '26';
+
+
     /*
         ☑️ Now, create a new variable called "myFullName" USING the "myFirstName" variable
         ☑️ Print a statement introducing yourself
     */
+        const myFullName = `${myFirstName} Noyahr`;
+        console.log (`Hi there my name is ${myFullName} and I am ${myAge} years old! `);
 
+        
     /*
         ☑️ Let's next write an if statement that prints 
             > "Im older than Aaron" if you're older than 28
@@ -17,12 +24,39 @@
             > Otherwise it should print "Im younger than Aaron"
             (You Should Have an If, and Else if, and an Else below -- not just a single console.log)
     */
+
+        const aaronAge = 28;
+
+        if (myAge > aaronAge){
+            console.log("Im older than Aaron");
+        }
+
+        else if (myAge == aaronAge){
+            console.log ("Im the same age as Aaron");
+        }
+
+        else {
+            console.log("Im younger than Aaron");
+        }
+    
     
     /*
         ☑️ Next, calculate whether your age is even or odd, and store it in a boolean variable "isEven"
         ☑️ Now write another if statement that prints "My age is even", or "My age is odd", 
           depending which is true
     */
+
+          const isEven = myAge % 2 ===0;
+
+          console.log(isEven);
+
+          if (isEven) {
+              console.log("My age is even");
+          }
+
+          else {
+              console.log("My age is odd");
+          }
 
     /*
         YOUR CODE SHOULD NOW PRINT 3 SENTENCES TO THE CONSOLE:
@@ -45,3 +79,25 @@
             -> if there's 0 or more than 1: "There ARE 2 active USERS"
             -> (note the difference)
      */
+
+            let activeUserCount = 2;
+
+            if (activeUserCount > 1){
+                console.log (`There ARE ${activeUserCount} active users.`);
+            }
+
+            else if (activeUserCount ==1){
+                console.log (`There IS ${activeUserCount} active user.`);
+            }
+
+            else {
+                console.log ("There are NO active users");
+            }
+
+
+
+
+
+
+
+
